74b4f7f4f0a96eead8a857a10aae6425c76cafd3
Vially Israel <viallyisrael@hotmail.com>
2021-05-03 21:21:11 -0300

    Four verbs with mocks

--- 03_RestWithAspNet5_WorkingWithDifferentVerbs/RestWithAspNet5/RestWithAspNet5/Model/Person.cs
+++ 03_RestWithAspNet5_WorkingWithDifferentVerbs/RestWithAspNet5/RestWithAspNet5/Model/Person.cs
@@ -9,7 +9,7 @@ namespace RestWithAspNet5.Model
     {
         public long Id { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; private set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
         public string Gender { get; set; }
     }
---
+++ 03_RestWithAspNet5_WorkingWithDifferentVerbs/RestWithAspNet5/RestWithAspNet5/Services/PersonServiceImplementation.cs
@@ -0,0 +1,66 @@
+﻿using RestWithAspNet5.Model;
+using RestWithAspNet5.Services.Implementation;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Services
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+       
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+
+            return people;
+        }
+
+
+        public Person FindById(long id)
+        {
+            return new Person {
+                Id = 1,
+                FirstName = "Vially",
+                LastName = "Israel",
+                Address = "Rua 1234",
+                Gender = "M"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+
+        private Person MockPerson(int id)
+        {
+            return new Person
+            {
+                Id = id+1,
+                FirstName = (id+1).ToString() +" Vially",
+                LastName = "Israel",
+                Address = "Rua 1234",
+                Gender = "M"
+            };
+        }
+
+
+    }
+}
