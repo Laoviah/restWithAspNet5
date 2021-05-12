a9d7124a0a39a83fb82df1dd6ee8aeeb395710b6
Vially Israel <viallyisrael@hotmail.com>
2021-05-11 22:06:41 -0300

    commit padrão

--- 07_RestWithAspNet5_Migration/RestWithAspNet5/RestWithAspNet5/Repository/Implementation/BookRepositoryImplementation.cs
+++ 07_RestWithAspNet5_Migration/RestWithAspNet5/RestWithAspNet5/Repository/Implementation/BookRepositoryImplementation.cs
@@ -3,7 +3,6 @@ using RestWithAspNet5.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithAspNet5.Repository.Implementation
 {
