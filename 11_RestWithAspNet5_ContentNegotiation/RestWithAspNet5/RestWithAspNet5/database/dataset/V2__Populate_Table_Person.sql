9d60abb18d8fce3390b81d17eda0d9edd82c06e7
Vially Israel <viallyisrael@hotmail.com>
2021-05-12 22:17:21 -0300

    contentnegotiation session end

---
+++ 11_RestWithAspNet5_ContentNegotiation/RestWithAspNet5/RestWithAspNet5/database/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+﻿INSERT IGNORE INTO `person` (`id`, `address`, `first_name`, `last_name`, `Gender`) VALUES
+	(1, 'São Paulo, Brazil', 'Ayrton', 'Senna', 'Male'),
+	(110, 'Rua 1234 ttt', 'Vially t', 'Israel t', 'Male'),
+	(1111, '3 Rua 1234 ttt', '2 Vially t', '2 Israel t', 'Male');
