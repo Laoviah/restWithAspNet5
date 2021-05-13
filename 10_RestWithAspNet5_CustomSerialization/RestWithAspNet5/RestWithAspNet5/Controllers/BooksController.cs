b944f8e41da5200e7942762783442e1e0c8b5f29
Vially Israel <viallyisrael@hotmail.com>
2021-05-12 22:04:10 -0300

    custom serialization

---
+++ 10_RestWithAspNet5_CustomSerialization/RestWithAspNet5/RestWithAspNet5/Controllers/BooksController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAspNet5.Business;
+using RestWithAspNet5.Data.VO;
+using RestWithAspNet5.Model;
+using Serilog;
+
+namespace RestWithAspNet5.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly ILogger<BooksController> _logger;
+        private IBooksBusiness _bookBusiness;
+
+        public BooksController(ILogger<BooksController> logger, IBooksBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _bookBusiness.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BookVO bookVO)
+        {
+            if (bookVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_bookBusiness.Create(bookVO));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] BookVO bookVO)
+        {
+            if (bookVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_bookBusiness.Update(bookVO));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+
+
+            return NoContent();
+        }
+    }
+}
---
+++ 10_RestWithAspNet5_CustomSerialization/RestWithAspNet5/RestWithAspNet5/Data/Converter/Contract/IParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet5.Data.Converter.Contract
+{
+    public interface IParser<Origin, Destination>
+    {
+        Destination Parse(Origin origin);
+        List<Destination> Parse(List<Origin> origins);
+    }
+}
