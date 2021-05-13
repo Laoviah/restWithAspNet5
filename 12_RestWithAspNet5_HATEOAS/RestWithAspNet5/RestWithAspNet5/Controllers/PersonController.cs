694b5cfdaa62b0c3183cbd01e77e410bdbee1fbb
Vially Israel <viallyisrael@hotmail.com>
2021-05-13 20:22:08 -0300

    HATEOAS session started

---
+++ 12_RestWithAspNet5_HATEOAS/RestWithAspNet5/RestWithAspNet5/Controllers/PersonController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAspNet5.Model;
+using RestWithAspNet5.Business;
+using RestWithAspNet5.Data.VO;
+
+namespace RestWithAspNet5.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] PersonVO personVO)
+        {
+            if (personVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_personBusiness.Create(personVO));
+        }
+        
+        [HttpPut]
+        public IActionResult Put([FromBody] PersonVO personVO)
+        {
+            if (personVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_personBusiness.Update(personVO));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+
+            
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ 12_RestWithAspNet5_HATEOAS/RestWithAspNet5/RestWithAspNet5/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace RestWithAspNet5.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutingContext context);
+        bool Enrich(ResultExecutingContext context);
+    }
+}
