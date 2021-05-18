756cc285d01cd25b082646d47111127141a14274
Vially Israel <viallyisrael@hotmail.com>
2021-05-17 22:06:44 -0300

    Starting authentication session

---
+++ 15_RestWithAspNet5_Autenticacao/RestWithAspNet5/RestWithAspNet5/Controllers/PersonController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithAspNet5.Model;
+using RestWithAspNet5.Business;
+using RestWithAspNet5.Data.VO;
+using RestWithAspNet5.Hypermedia.Filters;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<BookVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO personVO)
+        {
+            if (personVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_personBusiness.Create(personVO));
+        }
+        
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO personVO)
+        {
+            if (personVO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_personBusiness.Update(personVO));
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(long id)
+        {
+            _personBusiness.Delete(id);
+
+            
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ 15_RestWithAspNet5_Autenticacao/RestWithAspNet5/RestWithAspNet5/Hypermedia/ContentResponseEnricher.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
+using RestWithAspNet5.Hypermedia.Abstract;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet5.Hypermedia
+{
+    public abstract class ContentResponseEnricher<T> : IResponseEnricher where T : ISupportHypermedia
+    {
+
+        public ContentResponseEnricher()
+        {
+
+        }
+
+
+        public virtual bool CanEnrich(Type contetType)
+        {
+            return contetType == typeof(T) || contetType == typeof(List<T>);
+        }
+
+        protected abstract Task EnrichModel(T content, IUrlHelper urlHelper);
+        bool IResponseEnricher.CanEnrich(ResultExecutingContext response)
+        {
+
+            if (response.Result is OkObjectResult okObjectResult)
+            {
+                return CanEnrich(okObjectResult.Value.GetType());
+            }
+            return false;
+        }
+
+
+        public async Task Enrich(ResultExecutingContext response)
+        {
+            var urlHelper = new UrlHelperFactory().GetUrlHelper(response);
+
+            if (response.Result is OkObjectResult okObjectResult)
+            {
+                if (okObjectResult is T model)
+                {
+                    await EnrichModel(model, urlHelper);
+                }
+                else if (okObjectResult.Value is List<T> colletcion)
+                {
+                    ConcurrentBag<T> bag = new ConcurrentBag<T>(colletcion);
+
+                    Parallel.ForEach(bag, (element) =>
+                    {
+                        EnrichModel(element, urlHelper);
+                    });
+                }
+            }
+            await Task.FromResult<object>(null);
+        }
+
+
+    }
+}
