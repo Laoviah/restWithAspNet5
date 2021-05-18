1c1d5aba3e998b3da376a3bc03bcc5036e4bc154
Vially Israel <viallyisrael@hotmail.com>
2021-05-17 21:38:24 -0300

    cors

---
+++ 14_RestWithAspNet5_CORS/RestWithAspNet5/RestWithAspNet5/Business/Implementation/PersonBusinessImplementation.cs
@@ -0,0 +1,55 @@
+﻿using RestWithAspNet5.Data.Converter.Implementations;
+using RestWithAspNet5.Data.VO;
+using RestWithAspNet5.Model;
+using RestWithAspNet5.Repository.Generic;
+using System.Collections.Generic;
+
+namespace RestWithAspNet5.Business.Implementation
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+        private readonly PersonConverter _personConverter;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+            _personConverter = new PersonConverter();
+        }
+
+        public PersonVO FindById(long id)
+        {
+            return _personConverter.Parse(_repository.FindById(id));
+
+        }
+
+        public List<PersonVO> FindAll()
+        {
+            return _personConverter.Parse(_repository.FindAll());
+        }
+
+        public PersonVO Create(PersonVO personVO)
+        {
+            var personEntity = _personConverter.Parse(personVO);
+            personEntity = _repository.Create(personEntity);
+
+            return _personConverter.Parse(personEntity);
+        }
+
+        public PersonVO Update(PersonVO personVO)
+        {
+
+            var personEntity = _personConverter.Parse(personVO);
+            personEntity = _repository.Update(personEntity);
+
+            return _personConverter.Parse(personEntity);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+
+    }
+}
---
+++ 14_RestWithAspNet5_CORS/RestWithAspNet5/RestWithAspNet5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithAspNet5.Model.Base;
+using RestWithAspNet5.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithAspNet5.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private MySqlContext _context;
+        private DbSet<T> _dataset;
+
+        public GenericRepository(MySqlContext mySqlContext)
+        {
+            _context = mySqlContext;
+            _dataset = _context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                _context.Add<T>(item);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return item;
+        }
+
+        public T Update(T item)
+        {
+            var result = _dataset.SingleOrDefault(p => p.Id.Equals(item.Id)) ;
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+
+                    return result;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return null;
+
+        }
+
+        public void Delete(long id)
+        {
+            var result = _dataset.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _dataset.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+
+        public List<T> FindAll()
+        {
+            return _dataset.ToList();
+        }
+
+        public T FindById(long id)
+        {
+            var result = _dataset.SingleOrDefault(p => p.Id.Equals(id));
+
+            return result;
+        }
+
+
+
+        public bool Exists(long id)
+        {
+            return _dataset.Any(p => p.Id.Equals(id));
+        }
+    }
+}
