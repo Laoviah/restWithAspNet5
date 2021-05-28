934831b722fd4e621e7111b213c5c998b3f2e515
Vially Israel <viallyisrael@hotmail.com>
2021-05-27 21:15:38 -0300

    finished

---
+++ 16_RestWithAspNet5_VerbPatch/RestWithAspNet5/RestWithAspNet5/Repository/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using RestWithAspNet5.Model;
+using RestWithAspNet5.Repository.Generic;
+
+namespace RestWithAspNet5.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disabled(long id);
+
+    }
+}
---
+++ 16_RestWithAspNet5_VerbPatch/RestWithAspNet5/RestWithAspNet5/Repository/PersonRepository.cs
@@ -0,0 +1,40 @@
+﻿using RestWithAspNet5.Model;
+using RestWithAspNet5.Model.Context;
+using RestWithAspNet5.Repository.Generic;
+using System;
+using System.Linq;
+
+namespace RestWithAspNet5.Repository
+{
+    public class PersonRepository : GenericRepository<Person>, IPersonRepository
+    {
+
+        public PersonRepository(MySqlContext context) : base (context)
+        {
+
+        }
+
+        public Person Disabled(long id)
+        {
+            var user = _context.People.SingleOrDefault(p => p.Id.Equals(id));
+            if (user == null)
+            {
+                return null;
+            }
+
+            user.Enabled = false;
+            try
+            {
+                _context.Entry(user).CurrentValues.SetValues(user);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return user;
+        }
+    }
+}
